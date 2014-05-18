5b2079eb0c97faa00fb4c863b638f99d7209684b
Rotem Yaari <vmalloc@gmail.com>
2014-05-18 11:51:12 +0300

    Sync with pytest 3612:9a891617d5a2

---
+++ _pytest/pdb.py
@@ -0,0 +1,100 @@
+""" interactive debugging with PDB, the Python Debugger. """
+
+import pytest, py
+import sys
+
+def pytest_addoption(parser):
+    group = parser.getgroup("general")
+    group._addoption('--pdb',
+               action="store_true", dest="usepdb", default=False,
+               help="start the interactive Python debugger on errors.")
+
+def pytest_namespace():
+    return {'set_trace': pytestPDB().set_trace}
+
+def pytest_configure(config):
+    if config.getvalue("usepdb"):
+        config.pluginmanager.register(PdbInvoke(), 'pdbinvoke')
+
+    old = (py.std.pdb.set_trace, pytestPDB._pluginmanager)
+    def fin():
+        py.std.pdb.set_trace, pytestPDB._pluginmanager = old
+    py.std.pdb.set_trace = pytest.set_trace
+    pytestPDB._pluginmanager = config.pluginmanager
+    config._cleanup.append(fin)
+
+class pytestPDB:
+    """ Pseudo PDB that defers to the real pdb. """
+    _pluginmanager = None
+
+    def set_trace(self):
+        """ invoke PDB set_trace debugging, dropping any IO capturing. """
+        frame = sys._getframe().f_back
+        capman = None
+        if self._pluginmanager is not None:
+            capman = self._pluginmanager.getplugin("capturemanager")
+            if capman:
+                capman.suspendcapture(in_=True)
+            tw = py.io.TerminalWriter()
+            tw.line()
+            tw.sep(">", "PDB set_trace (IO-capturing turned off)")
+        py.std.pdb.Pdb().set_trace(frame)
+
+
+class PdbInvoke:
+    def pytest_exception_interact(self, node, call, report):
+        capman = node.config.pluginmanager.getplugin("capturemanager")
+        if capman:
+            capman.suspendcapture(in_=True)
+        _enter_pdb(node, call.excinfo, report)
+
+    def pytest_internalerror(self, excrepr, excinfo):
+        for line in str(excrepr).split("\n"):
+            sys.stderr.write("INTERNALERROR> %s\n" %line)
+            sys.stderr.flush()
+        tb = _postmortem_traceback(excinfo)
+        post_mortem(tb)
+
+
+def _enter_pdb(node, excinfo, rep):
+    # XXX we re-use the TerminalReporter's terminalwriter
+    # because this seems to avoid some encoding related troubles
+    # for not completely clear reasons.
+    tw = node.config.pluginmanager.getplugin("terminalreporter")._tw
+    tw.line()
+    tw.sep(">", "traceback")
+    rep.toterminal(tw)
+    tw.sep(">", "entering PDB")
+    tb = _postmortem_traceback(excinfo)
+    post_mortem(tb)
+    rep._pdbshown = True
+    return rep
+
+
+def _postmortem_traceback(excinfo):
+    # A doctest.UnexpectedException is not useful for post_mortem.
+    # Use the underlying exception instead:
+    if isinstance(excinfo.value, py.std.doctest.UnexpectedException):
+        return excinfo.value.exc_info[2]
+    else:
+        return excinfo._excinfo[2]
+
+
+def _find_last_non_hidden_frame(stack):
+    i = max(0, len(stack) - 1)
+    while i and stack[i][0].f_locals.get("__tracebackhide__", False):
+        i -= 1
+    return i
+
+
+def post_mortem(t):
+    pdb = py.std.pdb
+    class Pdb(pdb.Pdb):
+        def get_stack(self, f, t):
+            stack, i = pdb.Pdb.get_stack(self, f, t)
+            if f is None:
+                i = _find_last_non_hidden_frame(stack)
+            return stack, i
+    p = Pdb()
+    p.reset()
+    p.interaction(None, t)
---
+++ testing/test_tmpdir.py
@@ -0,0 +1,103 @@
+import py
+import pytest
+
+from _pytest.tmpdir import tmpdir, TempdirHandler
+
+def test_funcarg(testdir):
+    testdir.makepyfile("""
+            def pytest_generate_tests(metafunc):
+                metafunc.addcall(id='a')
+                metafunc.addcall(id='b')
+            def test_func(tmpdir): pass
+    """)
+    reprec = testdir.inline_run()
+    calls = reprec.getcalls("pytest_runtest_setup")
+    item = calls[0].item
+    # pytest_unconfigure has deleted the TempdirHandler already
+    config = item.config
+    config._tmpdirhandler = TempdirHandler(config)
+    item._initrequest()
+    p = tmpdir(item._request)
+    assert p.check()
+    bn = p.basename.strip("0123456789")
+    assert bn.endswith("test_func_a_")
+    item.name = "qwe/\\abc"
+    p = tmpdir(item._request)
+    assert p.check()
+    bn = p.basename.strip("0123456789")
+    assert bn == "qwe__abc"
+
+def test_ensuretemp(recwarn):
+    #pytest.deprecated_call(pytest.ensuretemp, 'hello')
+    d1 = pytest.ensuretemp('hello')
+    d2 = pytest.ensuretemp('hello')
+    assert d1 == d2
+    assert d1.check(dir=1)
+
+class TestTempdirHandler:
+    def test_mktemp(self, testdir):
+        config = testdir.parseconfig()
+        config.option.basetemp = testdir.mkdir("hello")
+        t = TempdirHandler(config)
+        tmp = t.mktemp("world")
+        assert tmp.relto(t.getbasetemp()) == "world0"
+        tmp = t.mktemp("this")
+        assert tmp.relto(t.getbasetemp()).startswith("this")
+        tmp2 = t.mktemp("this")
+        assert tmp2.relto(t.getbasetemp()).startswith("this")
+        assert tmp2 != tmp
+
+class TestConfigTmpdir:
+    def test_getbasetemp_custom_removes_old(self, testdir):
+        p = testdir.tmpdir.join("xyz")
+        config = testdir.parseconfigure("--basetemp=xyz")
+        b = config._tmpdirhandler.getbasetemp()
+        assert b == p
+        h = b.ensure("hello")
+        config._tmpdirhandler.getbasetemp()
+        assert h.check()
+        config = testdir.parseconfigure("--basetemp=xyz")
+        b2 = config._tmpdirhandler.getbasetemp()
+        assert b2.check()
+        assert not h.check()
+
+def test_basetemp(testdir):
+    mytemp = testdir.tmpdir.mkdir("mytemp")
+    p = testdir.makepyfile("""
+        import pytest
+        def test_1():
+            pytest.ensuretemp("hello")
+    """)
+    result = testdir.runpytest(p, '--basetemp=%s' % mytemp)
+    assert result.ret == 0
+    assert mytemp.join('hello').check()
+
+@pytest.mark.skipif(not hasattr(py.path.local, 'mksymlinkto'),
+                    reason="symlink not available on this platform")
+def test_tmpdir_always_is_realpath(testdir):
+    # the reason why tmpdir should be a realpath is that
+    # when you cd to it and do "os.getcwd()" you will anyway
+    # get the realpath.  Using the symlinked path can thus
+    # easily result in path-inequality
+    # XXX if that proves to be a problem, consider using
+    # os.environ["PWD"]
+    realtemp = testdir.tmpdir.mkdir("myrealtemp")
+    linktemp = testdir.tmpdir.join("symlinktemp")
+    linktemp.mksymlinkto(realtemp)
+    p = testdir.makepyfile("""
+        def test_1(tmpdir):
+            import os
+            assert os.path.realpath(str(tmpdir)) == str(tmpdir)
+    """)
+    result = testdir.runpytest("-s", p, '--basetemp=%s/bt' % linktemp)
+    assert not result.ret
+
+def test_tmpdir_too_long_on_parametrization(testdir):
+    testdir.makepyfile("""
+        import pytest
+        @pytest.mark.parametrize("arg", ["1"*1000])
+        def test_some(arg, tmpdir):
+            tmpdir.ensure("hello")
+    """)
+    reprec = testdir.inline_run()
+    reprec.assertoutcome(passed=1)
