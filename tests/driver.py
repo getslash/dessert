e24e5268acb6896b32a581579bb14137a2051fff
Rotem Yaari <vmalloc@gmail.com>
2017-03-26 16:21:23 +0300

    Add integration tests for messages

---
+++ tests/driver.py
@@ -0,0 +1,16 @@
+import sys
+
+if __name__ == "__main__":
+    [_, example_name] = sys.argv # pylint: disable=unbalanced-tuple-unpacking
+    import dessert
+
+    with dessert.rewrite_assertions_context():
+        import examples
+
+    func = getattr(examples, example_name)
+    try:
+        func()
+    except AssertionError as e:
+        print(e)
+    else:
+        sys.exit('Assertion not matched')
---
+++ tests/test_integration.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+import os
+import shutil
+import functools
+import pytest
+import subprocess
+import sys
+
+
+def test_example_code_simple_assertion(run_example):
+    assert run_example('no_message_simple_equality') == 'assert 1 == 2'
+
+
+def test_example_message_assertion(run_example):
+    assert run_example('with_message_simple_equality') == 'Here is an assertion message\nassert 1 == 2'
+
+
+
+@pytest.fixture
+def run_example(tmpdir):
+    here = os.path.dirname(__file__)
+    shutil.copy(os.path.join(here, 'driver.py'), str(tmpdir))
+    shutil.copy(os.path.join(here, 'examples.py'), str(tmpdir))
+
+    return functools.partial(_run, str(tmpdir.join('driver.py')))
+
+def _run(filename, example_name):
+    executable = sys.executable
+    output = subprocess.check_output([executable, filename, example_name], stderr=subprocess.STDOUT).decode('utf-8')
+    return output.strip()
